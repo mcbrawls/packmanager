fd041d2fb2e61f38ade982fdaaefc22f643ce93e
Daniel Orr <orrdaniel73@gmail.com>
2023-11-23 23:03:36 +0000

    Initial commit

---
+++ src/main/kotlin/net/mcbrawls/packmanager/PackManager.kt
@@ -0,0 +1,15 @@
+package net.mcbrawls.packmanager
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object PackManager : ModInitializer {
+    private val logger = LoggerFactory.getLogger("packmanager")
+
+    override fun onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        logger.info("Hello Fabric world!")
+    }
+}
